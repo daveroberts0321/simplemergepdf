5d148a02da0d530505e4891b4ef97c5a32ba9f43
daveroberts0321 <dave@tac-llc.com>
2026-02-25 17:46:29 -0600

    2502251746

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,13 @@
 </script>
 
 <svelte:head>
+<!-- Privacy-friendly analytics by Plausible -->
+<script async src="https://plausible.io/js/pa-axRh61JaAgdfiIveytKB3.js"></script>
+<script>
+  window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+  plausible.init()
+</script>
+
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
 	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
